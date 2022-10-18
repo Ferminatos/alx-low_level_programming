97b6334cec5f5b5f96eebfe660c221643f1a7688
Bruke <BrukeD12@gmail.com>
2022-10-18 13:00:09 -0700

    functions and nested loops eight

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: int type number
+ * Return: return value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int lr;
+
+	if (n < 0)
+	{
+		lr = -1 * (n % 10);
+		return (lr);
+	}
+
+	else
+	{
+		lr = n % 10;
+		return (lr);
+	}
+}
