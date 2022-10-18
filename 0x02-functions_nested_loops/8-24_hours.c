03924e513c2404a3b1842b6b866612f2832556bb
Bruke <BrukeD12@gmail.com>
2022-10-18 13:31:41 -0700

    functions and nested loops nine

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int hr;
+	int mn;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (mn = 0; mn <= 59; mn++)
+		{
+			if (hr < 10)
+				_putchar('0' + hr);
+
+			else
+				_putchar(hr);
+
+			_putchar(':');
+
+			if (mn < 10)
+				_putchar('0' + mn);
+
+			else
+				_putchar(mn);
+
+			_putchar('\n');
+		}
+	}
+}
