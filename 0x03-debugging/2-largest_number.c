d2b4a856c42d13703496f5f4469223150c1787c9
Bruke <BrukeD12@gmail.com>
2022-10-20 14:06:03 -0700

    dubugging three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,19 +14,13 @@ int largest_number(int a, int b, int c)
 	int count = 0;
 
 	if (a < 0)
-	{
 		count++;
-	}
 
 	if (b < 0)
-	{
 		count++;
-	}
 
 	if (c < 0)
-	{
 		count++;
-	}
 
 	if (count == 0)
 	{
@@ -38,40 +32,8 @@ int largest_number(int a, int b, int c)
 		else
 			largest = c;
 	}
-
-	else if (count == 1)
-	{
-		if (a < 0)
-		{
-			if (b > c)
-				largest = b;
-			else
-				largest = c;
-		}
-		if (b < 0)
-		{
-			if (a > c)
-				largest = a;
-			else
-				largest = c;
-		}
-		else
-		{
-			if (a > b)
-				largest = a;
-			else
-				largest = b;
-		}
-	}
-
-	else if (count == 2)
-	{
-		/* 2 of the numbers are negative*/
-	}
-
 	else
-	{
-		/* 3 of the numbers are negative*/
-	}
+		largest = cal_negatives(count,a,b,c);
+
 	return (largest);
 }
