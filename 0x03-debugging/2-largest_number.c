79c7f12107be012ab0d9dd8b0ea0852d9633788a
Bruke <BrukeD12@gmail.com>
2022-10-20 13:25:49 -0700

    dubugging two

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,28 +11,67 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+	int count = 0;
 
-
-	if (a >= b && b >= c)
+	if (a < 0)
 	{
-
-		largest = a;
-
+		count++;
 	}
 
-	else if (b >= a && a >= c)
+	if (b < 0)
 	{
+		count++;
+	}
 
-		largest = b;
+	if (c < 0)
+	{
+		count++;
+	}
 
+	if (count == 0)
+	{
+		if (a > b && b > c)
+			largest = a;
+
+		else if (b > a && a > c)
+			largest = b;
+		else
+			largest = c;
+	}
+
+	else if (count == 1)
+	{
+		if (a < 0)
+		{
+			if (b > c)
+				largest = b;
+			else
+				largest = c;
+		}
+		if (b < 0)
+		{
+			if (a > c)
+				largest = a;
+			else
+				largest = c;
+		}
+		else
+		{
+			if (a > b)
+				largest = a;
+			else
+				largest = b;
+		}
+	}
+
+	else if (count == 2)
+	{
+		/* 2 of the numbers are negative*/
 	}
 
 	else
 	{
-
-		largest = c;
-
+		/* 3 of the numbers are negative*/
 	}
-
 	return (largest);
 }
