d8498a75777f4ce7e91a05189b4e505daa2edddd
Bruke <BrukeD12@gmail.com>
2022-10-20 14:24:54 -0700

    dubgging three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,30 +10,46 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
-	int count = 0;
+	int largest, count = 0;
 
 	if (a < 0)
 		count++;
-
 	if (b < 0)
 		count++;
-
 	if (c < 0)
 		count++;
-
 	if (count == 0)
 	{
 		if (a > b && b > c)
 			largest = a;
-
 		else if (b > a && a > c)
 			largest = b;
 		else
 			largest = c;
 	}
-	else
-		largest = cal_negatives(count,a,b,c);
-
+	else if (count == 1)
+	{
+		if (a < 0)
+		{
+			if (b > c)
+				largest = b;
+			else
+				largest = c;
+		}
+		if (b < 0)
+		{
+			if (a > c)
+				largest = a;
+			else
+				largest = c;
+		}
+		else
+		{
+			if (a > b)
+				largest = a;
+			else
+				largest = b;
+		}
+	}
 	return (largest);
 }
