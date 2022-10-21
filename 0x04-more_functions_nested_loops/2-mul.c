a8d080cc85553243da4eec173e81d3759c7a87b3
Bruke <BrukeD12@gmail.com>
2022-10-21 06:55:11 -0700

    more functions and nested loops three

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first intger variable
+ * @b: second intger varible
+ * Return: n multiple of the two integers
+ */
+
+int mul(int a, int b)
+{
+	return  (a * b);
+}
