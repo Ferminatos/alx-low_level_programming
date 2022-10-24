f3c20d637e1754cdc6dd55a1f2bd2b95adf87859
Bruke <BrukeD12@gmail.com>
2022-10-24 11:22:54 -0700

    Pointers, arrays and strings six

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,21 @@
 
 void rev_string(char *s)
 {
-	int l;
-	int count;
-	int index = 0;
+	int l, count, index = 0;
+	char h;
 
 	for (count = 0; s[count] != 0; count++)
 	{
 
 	}
 
-	for (l = count - 1; l >= 0; l--)
+	for (l = count - 1; index < l / 2; l--)
 	{
+		h = s[index];
 		s[index] = s[l];
+		s[l] = h;
 		index++;
 	}
+
+
 }
