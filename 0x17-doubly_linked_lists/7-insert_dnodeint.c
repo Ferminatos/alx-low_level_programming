697885d6570a41b2453ccc982d207991db66f990
Bruke <BrukeD12@gmail.com>
2023-01-12 13:47:12 -0800

    doubly linked lists eight

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,51 +10,42 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int c;
-	dlistint_t *tmp, *prev, *new;
+	dlistint_t *tmp;
+	dlistint_t *new;
+	unsigned int i;
 
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
+	if (h == NULL)
 		return (NULL);
-
-	new->n = n;
-
-	for (tmp = *h, c = 1; tmp && c < idx; c++, tmp = tmp->next)
-		prev = tmp;
-
 	if (idx == 0)
-	{
-		*h = new; new->prev = NULL;
-		new->next = (tmp == NULL) ? NULL : tmp;
-		return (new);
-	}
-	if (idx == 1)
-	{
-		prev = *h;
-		tmp = ((*h)->next == NULL) ? NULL : (*h)->next;
-		new->prev = prev; new->next = tmp; prev->next = new;
-		if (tmp)
-			tmp->prev = new;
+		return (add_dnodeint(h, n));
 
-		return (new);
-	}
-	if (idx == c && tmp == NULL)
-	{
-		if (prev != NULL)
-		{
-			new->prev = prev; new->next = NULL;
-			prev->next = new; return (new);
+	tmp = *h;
+	i = 0;
 
-		}
-		free(new); return (NULL);
-	}
-	else if (idx != c && tmp == NULL)
+	while (i < idx - 1)
 	{
-		free(new); return (NULL);
+		if (tmp == NULL)
+			return (NULL);
+		i++;
+		tmp = tmp->next;
 	}
-	prev = tmp; tmp = tmp->next; new->prev = prev;
-	new->next = tmp; prev->next = new; tmp->prev = new;
+
+	if (tmp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+
+	else
+	{
+			new = malloc(sizeof(dlistint_t));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
+			new->next = tmp->next;
+			tmp->next->prev = new;
+			tmp->next = new;
+			new->prev = tmp;
+	}
 
 	return (new);
 }
