e20630ad47e200e6ce4cd450ff8c15887c48c4b9
Bruke <BrukeD12@gmail.com>
2023-11-03 13:49:32 -0700

    Malloc tasks

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenate 2 strings, only n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes to include of s2
+ * Return: NULL if fail, else pointer to malloc memory
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *strDup;
+	int i;
+	unsigned int j;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	i = 0;
+
+	while (s1[i] != '\0')
+		i++;
+
+	strDup = malloc(sizeof(char) * (i + n + 1));
+
+	if (strDup == NULL)
+		return (NULL);
+
+	i = j = 0;
+
+	while (s1[i] != '\0')
+	{
+		strDup[i] = s1[i];
+		i++;
+	}
+
+	while (j < n && s2[j] != '\0')
+	{
+		strDup[i] = s2[j];
+		i++, j++;
+
+	}
+	strDup[i] = '\0';
+
+	return (strDup);
+}
