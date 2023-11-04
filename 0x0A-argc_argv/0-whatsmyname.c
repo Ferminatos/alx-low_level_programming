01e371f4c247ee0b5b875d04f10e9df2affad8d4
Bruke <BrukeD12@gmail.com>
2023-11-04 01:48:23 -0700

    argc, argv tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints programs name, followed by a new line.
+ * @argc: count of arguments
+ * @argcv: array of pointers to the strings
+ * Return: Always (0) Success
+ */
+
+int main(int argc, char **argcv)
+{
+	(void) argc;
+
+	printf("%s\n", argcv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# The folder contains exercise files that illustrate arguments to main argc, argv in c
